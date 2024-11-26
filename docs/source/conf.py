ca2f4f0eebc0a4b4fd68d685c41be753feb028c7
reyna-abhyankar <forvirenra@gmail.com>
2024-11-25 17:34:31 -0800

    Update docs author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'Cognify'
 copyright = '2024, GenseeAI Team'
-author = 'GenseeAI Team'
+author = 'Cognify Team'
 release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
