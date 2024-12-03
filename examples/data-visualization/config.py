767d86703800341d5203a01a55dd4e33feb3b8f9
Reyna Abhyankar <forvirenra@gmail.com>
2024-12-03 23:02:41 +0000

    Add links to original data visualization

--- examples/data-visualization/config.py
+++ examples/data-visualization/config.py
@@ -3,20 +3,16 @@ import json
 import numpy as np
 import os
 
-# ================= Data Loader =================
 @cognify.register_data_loader
 def load_data():
     def load_from_file(input_file):
-        data_path = 'benchmark_data'
         # open the json file 
-        data = json.load(open(f'{data_path}/{input_file}'))
+        data = json.load(open(input_file))
         
         all_data = []
         for item in data:
             novice_instruction = item['simple_instruction']
-            expert_instruction = item['expert_instruction']
-            example_id = item['id'] 
-            # directory_path = f'examples/IR_matplot_agent/new_opt_runs_test_data_mini_reason'
+            example_id = item['id']
             directory_path = f'opt_runs'
 
             if not os.path.exists(directory_path):
@@ -26,14 +22,14 @@ def load_data():
                 'query': novice_instruction,
                 "directory_path": directory_path,
                 "example_id": example_id,
-                "input_path": f'{data_path}/data/{example_id}',
+                "input_path": f'benchmark_data/data/{example_id}',
             }
             label = {"ground_truth": f"benchmark_data/ground_truth/example_{example_id}.png"}
             all_data.append((input, label))
         return all_data
             
-    all_train = load_from_file('train_data.json')
-    test_data = load_from_file('test_data.json')
+    all_train = load_from_file('benchmark_split/train_data.json')
+    test_data = load_from_file('benchmark_split/test_data.json')
     train_indices = np.random.choice(range(len(all_train)), 40, replace=False).tolist()
     eval_indices = list(set(range(len(all_train))) - set(train_indices))
     
