0b56c4dbd5b6a677e87ea9c354255eafcc8bdbc2
Reyna Abhyankar <forvirenra@gmail.com>
2024-12-04 22:37:11 +0000

    Move evaluators into hub

--- cognify/__init__.py
+++ cognify/__init__.py
@@ -12,8 +12,8 @@ from .frontends.dspy.connector import PredictModel, as_predict
 from .frontends.langchain.connector import RunnableModel, as_runnable
 
 from cognify import llm, optimizer
-from cognify.optimizer.evaluation import metric
-from cognify.optimizer.evaluation.metric import f1_score_set
+from cognify.hub import metric
+from cognify.hub.metric import f1_score_set
 
 from cognify.run.evaluate import evaluate, load_workflow
 from cognify.run.optimize import optimize
---
+++ cognify/hub/metric/__init__.py
@@ -0,0 +1,4 @@
+from .f1_score import f1_score_set, f1_score_ordered, f1_score_str
+from .llm_judge import llm_judge_generic
+from .code_humaneval import humaneval_evaluator
+from .match import exact_match
