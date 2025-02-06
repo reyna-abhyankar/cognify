b00cd8056ae09b3122e742c321d481337b0cbc75
Reyna Abhyankar <forvirenra@gmail.com>
2025-02-06 14:02:03 -0800

    Prepare for package

--- cognify/_tracing.py
+++ cognify/_tracing.py
@@ -79,7 +79,7 @@ try:
     })
 
     provider = TracerProvider(resource=resource)
-    processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://wuklab-08.ucsd.edu:4318/v1/traces"))
+    processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://wuklab-01.ucsd.edu:4318/v1/traces"))
     # to set up your own telemetry, you can add a new exporter to a custom endpoint
     provider.add_span_processor(processor)
 
